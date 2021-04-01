7446ec5aa7aba590e5e9caa51f7f029dc9ddbb27
Juan Isaza <jisazamp@gmail.com>
2021-04-01 14:17:42 -0500

    Componente de Spinner

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,19 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 import UserItem from './UserItem';
+import Spinner from '../layout/Spinner';
+import PropTypes from 'prop-types';
 
-class Users extends Component {
-  render() {
-    const users = this.props.users.map((user) => {
-      return <UserItem key={user.id} user={user} />;
-    });
-    return <div style={userStyle}>{users}</div>;
-  }
-}
+const Users = ({ users, loading }) => {
+  const usuarios = users.map((user) => {
+    return <UserItem key={user.id} user={user} />;
+  });
+
+  return loading ? <Spinner /> : <div style={userStyle}>{usuarios}</div>;
+};
 
 const userStyle = {
   display: 'grid',
   gridTemplateColumns: 'repeat(3, 1fr)',
   gridGap: '1rem',
+};
+
+Users.propTypes = {
+  users: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired,
 };
 
 export default Users;
