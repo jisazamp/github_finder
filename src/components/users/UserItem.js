e8bbfa70bad73832a38edba0abfedb9bf9c15f3c
Juan Isaza <jisazamp@gmail.com>
2021-04-01 13:48:23 -0500

    Refactor para uso de Componentes Funcionales

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,32 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-class UserItem extends Component {
-  //   state = {
-  //     id: 'id',
-  //     login: 'mojombo',
-  //     avatar_url: 'https://avatars.githubusercontent.com/u/1?v=4',
-  //     html_url: 'https://github.com/mojombo',
-  //   };
-
-  render() {
-    const { avatar_url, login, html_url } = this.props.user;
-    return (
-      <div className='card text-center'>
-        <img
-          src={avatar_url}
-          alt='Avatar'
-          className='round-img'
-          style={{ width: '60px' }}
-        />
-        <h3>{login}</h3>
-        <div>
-          <a href={html_url} className='btn btn-dark btn-sm my-1'>
-            More
-          </a>
-        </div>
+const UserItem = ({ user: { avatar_url, login, html_url } }) => {
+  return (
+    <div className='card text-center'>
+      <img
+        src={avatar_url}
+        alt='Avatar'
+        className='round-img'
+        style={{ width: '60px' }}
+      />
+      <h3>{login}</h3>
+      <div>
+        <a
+          href={html_url}
+          target='_blank'
+          rel='noreferrer'
+          className='btn btn-dark btn-sm my-1'
+        >
+          More
+        </a>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+UserItem.propTypes = {
+  user: PropTypes.object.isRequired,
+};
 
 export default UserItem;
