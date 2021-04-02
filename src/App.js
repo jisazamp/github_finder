d17d8dce76cd87fcc77ab372ac1cacef0ca862b5
Juan Isaza <jisazamp@gmail.com>
2021-04-02 14:01:07 -0500

    Componente de User

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import User from './components/users/User';
 import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
 import About from './components/pages/About';
@@ -11,6 +12,7 @@ import './App.css';
 class App extends Component {
   state = {
     users: [],
+    user: {},
     loading: false,
     alert: null,
   };
@@ -38,6 +40,18 @@ class App extends Component {
     this.setState({ users: res.data.items, loading: false });
   };
 
+  // Buscar un solo usuario de Github
+  getUser = async (username) => {
+    this.setState({ loading: true });
+
+    const res = await axios.get(
+      `https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&
+    client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({ user: res.data, loading: false });
+  };
+
   clearUsers = async () => {
     if (this.state.users.length === 0) return;
     this.setState({ users: [], loading: false });
@@ -52,7 +66,7 @@ class App extends Component {
 
   // Método del Lifecycle de React render()
   render() {
-    const { loading, users } = this.state;
+    const { loading, users, user } = this.state;
 
     return (
       <Router>
@@ -77,6 +91,18 @@ class App extends Component {
                 )}
               />
               <Route exact path='/about' component={About} />
+              <Route
+                exact
+                path='/user/:login'
+                render={(props) => (
+                  <User
+                    {...props}
+                    getUser={this.getUser}
+                    user={user}
+                    loading={loading}
+                  />
+                )}
+              />
             </Switch>
           </div>
         </div>
