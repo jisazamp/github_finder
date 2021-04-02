9c04a17b0d9f767526f0d59f769dfc4d49b968f3
Juan Isaza <jisazamp@gmail.com>
2021-04-02 14:47:26 -0500

    Componente de Repos

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ class App extends Component {
     user: {},
     loading: false,
     alert: null,
+    repos: [],
   };
 
   async componentDidMount() {
@@ -52,6 +53,19 @@ class App extends Component {
     this.setState({ user: res.data, loading: false });
   };
 
+  // Buscar los repositorios del usuario
+  getUserRepos = async (username) => {
+    this.setState({ loading: true });
+
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc
+      &client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&
+  client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({ repos: res.data, loading: false });
+  };
+
   clearUsers = async () => {
     if (this.state.users.length === 0) return;
     this.setState({ users: [], loading: false });
@@ -66,7 +80,7 @@ class App extends Component {
 
   // Método del Lifecycle de React render()
   render() {
-    const { loading, users, user } = this.state;
+    const { loading, users, user, repos } = this.state;
 
     return (
       <Router>
@@ -98,7 +112,9 @@ class App extends Component {
                   <User
                     {...props}
                     getUser={this.getUser}
+                    getUserRepos={this.getUserRepos}
                     user={user}
+                    repos={repos}
                     loading={loading}
                   />
                 )}
