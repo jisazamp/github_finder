e5a9206ca9fb9334e61dc02f4e40b611007a8262
Juan Isaza <jisazamp@gmail.com>
2021-04-02 00:58:19 -0500

    React Router DOM

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+
+const About = () => {
+  return (
+    <Fragment>
+      <h1>Acerca De Este Proyecto</h1>
+      <p>Aplicación para buscar usuarios de GitHub</p>
+      <p>Versión: 1.0.0</p>
+    </Fragment>
+  );
+};
+
+export default About;
