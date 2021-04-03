492fe75680b3aea6b00d3d22c8b567baa4995b6d
Juan Isaza <jisazamp@gmail.com>
2021-04-02 19:12:28 -0500

    Componente de NotFound

---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1>No encontrado</h1>
+      <p className='lead'>La página que estás buscando no existe.</p>
+    </div>
+  );
+};
+
+export default NotFound;
